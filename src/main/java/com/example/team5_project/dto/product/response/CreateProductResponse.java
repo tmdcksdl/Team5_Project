8f450e32c694986c7118d635b42582e374d77a83
flashback7 <97232474+seoho1@users.noreply.github.com>
2025-02-02 16:41:43 +0900

    feat : add product_id response 값에 id 값을 누락시켜서 추가하였습니다.

--- src/main/java/com/example/team5_project/dto/product/response/CreateProductResponse.java
+++ src/main/java/com/example/team5_project/dto/product/response/CreateProductResponse.java
@@ -1,4 +1,4 @@
 package com.example.team5_project.dto.product.response;
 
-public record CreateProductResponse(String name, int price, int stock) {
+public record CreateProductResponse(Long product_id, String name, int price, int stock) {
 }
