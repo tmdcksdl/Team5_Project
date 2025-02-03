a03dbaef492cca5b108c7a0504fcf781718f6a17
flashback7 <97232474+seoho1@users.noreply.github.com>
2025-02-03 14:30:06 +0900

    refactor : api url 통일 접미에 product, store controller 에 api 추가함

--- src/main/java/com/example/team5_project/controller/StoreController.java
+++ src/main/java/com/example/team5_project/controller/StoreController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/stores")
+@RequestMapping("/api/stores")
 @RequiredArgsConstructor
 public class StoreController {
 
