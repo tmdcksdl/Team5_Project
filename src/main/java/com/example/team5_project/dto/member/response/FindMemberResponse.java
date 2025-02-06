432f3c097692c26e7c3bb08306ac22c702412051
tmdcksdl <tmdcksdl99@naver.com>
2025-02-06 09:48:26 +0900

    refactor: Member DTO 리팩토링
    - 기존 class 기반 DTO를 record로 변경

--- src/main/java/com/example/team5_project/dto/member/response/FindMemberResponse.java
+++ src/main/java/com/example/team5_project/dto/member/response/FindMemberResponse.java
@@ -2,21 +2,14 @@ package com.example.team5_project.dto.member.response;
 
 import com.example.team5_project.common.enums.Gender;
 import com.example.team5_project.common.enums.UserType;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
 import java.time.LocalDateTime;
 
-@Getter
-@AllArgsConstructor
-public class FindMemberResponse {
+public record FindMemberResponse(
+        Long id, String name,
+        String email, Gender gender,
+        String address, UserType userType,
+        LocalDateTime createdAt, LocalDateTime updatedAt
+) {
 
-    private Long id;
-    private String name;
-    private String email;
-    private Gender gender;
-    private String address;
-    private UserType user_type;
-    private LocalDateTime create_at;
-    private LocalDateTime updated_at;
 }
