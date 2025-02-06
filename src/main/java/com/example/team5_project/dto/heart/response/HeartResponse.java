6d031cef6ed2adcbdba0143598519769d31ff117
tmdcksdl <tmdcksdl99@naver.com>
2025-02-06 09:53:56 +0900

    refactor: Heart DTO 리팩토링
    - 기존 class 기반 DTO를 record로 변경

--- src/main/java/com/example/team5_project/dto/heart/response/HeartResponse.java
+++ src/main/java/com/example/team5_project/dto/heart/response/HeartResponse.java
@@ -1,17 +1,12 @@
 package com.example.team5_project.dto.heart.response;
 
 import com.example.team5_project.dto.product.response.ReadProductResponse;
+
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
-@AllArgsConstructor
-public class HeartResponse {
-
-    private final Long id;
-    private final Long memberId;
-    private final ReadProductResponse product;
-    private final LocalDateTime createdAt;
+public record HeartResponse(
+        Long id, Long memberId,
+        ReadProductResponse product, LocalDateTime createdAt
+) {
 
 }
